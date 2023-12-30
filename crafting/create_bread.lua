050e5da1d2d7423dc339a8d1265b6b09aed87095
R.Noto <rikitonoto@gmail.com>
2023-12-31 02:33:07 +0900

    fix bread bug

--- crafting/create_bread.lua
+++ crafting/create_bread.lua
@@ -1,25 +1,24 @@
 
 while true do
   sleep(0.1)
-  redstone.setOutput("back", true)
   turtle.select(1)
   wheat_count = turtle.getItemCount(wheat_slot)
-  if wheat_count <= 3 then
-    goto continue
+  if wheat_count > 3 then
+    redstone.setOutput("back", true)
+    sleep(0.1)
+    turtle.transferTo(2, math.floor(wheat_count / 3))
+    turtle.transferTo(3, math.floor(wheat_count / 3))
+
+    turtle.craft()
+
+    turtle.select(1)
+    turtle.dropDown()
+
+    turtle.select(2)
+    turtle.dropDown()
+
+    turtle.select(3)
+    turtle.dropDown()
+    redstone.setOutput("back", false)
   end
-  turtle.transferTo(2, math.floor(wheat_count / 3))
-  turtle.transferTo(3, math.floor(wheat_count / 3))
-
-  turtle.craft()
-
-  turtle.select(1)
-  turtle.dropDown()
-
-  turtle.select(2)
-  turtle.dropDown()
-
-  turtle.select(3)
-  turtle.dropDown()
-  redstone.setOutput("back", false)
-	::continue::
 end

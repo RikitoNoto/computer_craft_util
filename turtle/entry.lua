303958dc89ee70a506b5d093914c7159d0ff7484
R.Noto <rikitonoto@gmail.com>
2024-01-09 23:14:50 +0900

    create the mothod of file read without lf

---
+++ turtle/entry.lua
@@ -0,0 +1,12 @@
+local function read_trim_lf(path)
+  local file = fs.open(path, "r")
+  local result = ""
+  while true do
+    local line = file.readLine()
+    if line == nil then
+      return result
+    end
+
+    result = result .. line
+  end
+end
